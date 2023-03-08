2b247e22fff0c90fb5a28d92139c18ad1454b465
HBIbidunni <ayodunibidunni@gmail.com>
2023-03-07 20:20:58 -0800

    All 0x01. Python - if/else, loops, functions tasks

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Author - Bamidele Adefolaju
+
+def remove_char_at(str, n):
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
+
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author -Bamidele Adefolaju
+
+def pow(a, b):
+    return (a ** b)
+
